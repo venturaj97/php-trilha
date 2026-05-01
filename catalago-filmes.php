2e3b5d72f431dc68dfa32efe97f2835f75f17381
João Victor Ventura Mendes <joaoventura97@outlook.com>
2026-05-01 17:33:37 -0300

    acumulador

--- catalago-filmes.php
+++ catalago-filmes.php
@@ -5,7 +5,14 @@ echo "Catalago de filmes
 
 $nomeFilme = "Top Gun - Maverick";
 $anoLancamento = 2022;
-$notaFilme = 8.8;
+$somaDeNotas = 8.8;
+$somaDeNotas += 7.5;
+$somaDeNotas += 9;
+$somaDeNotas += 8.5;
+$somaDeNotas += 6;
+
+
+$notaFilme = $somaDeNotas / 5;
 $incluidoNoPlano = true;
 
-echo "$nomeFilme";
+echo "$notaFilme";
