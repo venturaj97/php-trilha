009c311963e0a10653527be28e2236952240e294
João Victor Ventura Mendes <joaoventura97@outlook.com>
2026-05-01 18:09:47 -0300

    conehcendo

--- catalago-filmes.php
+++ catalago-filmes.php
@@ -4,7 +4,7 @@ echo "Catalago de filmes
 ";
 
 $nomeFilme = "Top Gun - Maverick";
-$anoLancamento = 2022;
+$anoLancamento = $argv[1] ?? 2022;
 $somaDeNotas = 8.8;
 $somaDeNotas += 7.5;
 $somaDeNotas += 9;
@@ -15,4 +15,4 @@ $somaDeNotas += 6;
 $notaFilme = $somaDeNotas / 5;
 $incluidoNoPlano = true;
 
-echo "$notaFilme";
+echo "O filme $nomeFilme foi lançado em $anoLancamento e tem uma nota de $notaFilme";
